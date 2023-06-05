43839c62700c224a374318302a6a96acd5efb70c
Insheloots <divher566@gmail.com>
2023-06-05 02:02:27 -0500

    Optimizations API call

--- src/components/Search/apiUtils.js
+++ src/components/Search/apiUtils.js
@@ -2,6 +2,7 @@ const fetchCharacters = async () => {
     const URL_API_PEOPLE = "https://swapi.dev/api/people/";
     let allCharacters = [];
     let nextUrl = URL_API_PEOPLE;
+
     try {
         while (nextUrl) {
             const response = await fetch(nextUrl);
@@ -10,31 +11,24 @@ const fetchCharacters = async () => {
             nextUrl = data.next;
         }
 
-        const mappedCharacters = await Promise.all(
-            allCharacters.map(async (character) => {
-                const homeworldData = await fetchCharacterData(character.homeworld);
-                const filmsData = await Promise.all(
-                    character.films.map(async (film) => {
-                        const filmData = await fetchCharacterData(film);
-                        return filmData;
-                    })
-                );
+        const characterPromises = allCharacters.map(async (character) => {
+            const homeworldData = await fetchCharacterData(character.homeworld);
+            const filmsData = await fetchFilmData(character.films);
+            return {
+                name: character.name,
+                height: character.height,
+                mass: character.mass,
+                hair_color: character.hair_color,
+                skin_color: character.skin_color,
+                eye_color: character.eye_color,
+                gender: character.gender,
+                birth_year: character.birth_year,
+                homeworld: homeworldData.name,
+                films: filmsData.map((film) => film.title),
+            };
+        });
 
-                return {
-                    name: character.name,
-                    height: character.height,
-                    mass: character.mass,
-                    hair_color: character.hair_color,
-                    skin_color: character.skin_color,
-                    eye_color: character.eye_color,
-                    gender: character.gender,
-                    birth_year: character.birth_year,
-                    homeworld: homeworldData.name,
-                    films: filmsData.map((film) => film.title),
-                };
-            })
-        );
-
+        const mappedCharacters = await Promise.all(characterPromises);
         return mappedCharacters;
     } catch (error) {
         throw error;
@@ -46,6 +40,21 @@ const fetchCharacterData = async (url) => {
         const response = await fetch(url);
         const data = await response.json();
         return data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const fetchFilmData = async (filmUrls) => {
+    try {
+        const filmPromises = filmUrls.map(async (url) => {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        });
+
+        const filmsData = await Promise.all(filmPromises);
+        return filmsData;
     } catch (error) {
         throw error;
     }
